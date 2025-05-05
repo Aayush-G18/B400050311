bab51f5c3fc800290d96852d94056907b1b65718
Aayush-G18 <91402088+Aayush-G18@users.noreply.github.com>
2025-05-05 07:43:48 +0530

    Update 3_parallel_reduction.cpp

--- HPC/3_parallel_reduction.cpp
+++ HPC/3_parallel_reduction.cpp
@@ -1,3 +1,4 @@
+
 /*
   Windows does not support user defined reductions.
   This program may not run on MVSC++ compilers for Windows.
@@ -6,9 +7,10 @@
 
 #include<iostream>
 #include<omp.h>
-
+#include<numeric>
+#include<algorithm>
 using namespace std;
-int minval(int arr[], int n){
+int minval(long int arr[], int n){
   int minval = arr[0];
   #pragma omp parallel for reduction(min : minval)
     for(int i = 0; i < n; i++){
@@ -17,8 +19,8 @@ int minval(int arr[], int n){
   return minval;
 }
 
-int maxval(int arr[], int n){
-  int maxval = arr[0];
+long int maxval(long int arr[], int n){
+  long int maxval = arr[0];
   #pragma omp parallel for reduction(max : maxval)
     for(int i = 0; i < n; i++){
       if(arr[i] > maxval) maxval = arr[i];
@@ -26,8 +28,8 @@ int maxval(int arr[], int n){
   return maxval;
 }
 
-int sum(int arr[], int n){
-  int sum = 0;
+long int sum(long int arr[], int n){
+  long int sum = 0;
   #pragma omp parallel for reduction(+ : sum)
     for(int i = 0; i < n; i++){
       sum += arr[i];
@@ -35,16 +37,49 @@ int sum(int arr[], int n){
   return sum;
 }
 
-int average(int arr[], int n){
+long int average(long int arr[], int n){
   return (double)sum(arr, n) / n;
+}
+//Sequential
+
+int seq_minval(long int arr[], int n) {
+  return *min_element(arr, arr + n);
+}
+
+long int seq_maxval(long int arr[], int n) {
+  return *max_element(arr, arr + n);
+}
+
+long int seq_sum(long int arr[], int n) {
+  return accumulate(arr, arr + n, 0);
+}
+
+double seq_average(long int arr[], int n) {
+  return (double)seq_sum(arr, n) / n;
 }
 
 int main(){
-  int n = 5;
-  int arr[] = {1,2,3,4,5};
-  cout << "The minimum value is: " << minval(arr, n) << '\n';
-  cout << "The maximum value is: " << maxval(arr, n) << '\n';
-  cout << "The summation is: " << sum(arr, n) << '\n';
-  cout << "The average is: " << average(arr, n) << '\n';
+  int n = 1e8;
+  long int* arr=new long int[n];
+  for(int i = 0, j = n; i < n; i++, j--) arr[i] = j;
+  double start_time,end_time;
+  start_time = omp_get_wtime();
+  cout << "Sequential Min: " << seq_minval(arr, n) << '\n';
+  cout << "Sequential Max: " << seq_maxval(arr, n) << '\n';
+  cout << "Sequential Sum: " << seq_sum(arr, n) << '\n';
+  cout << "Sequential Avg: " << seq_average(arr, n) << '\n';
+  end_time = omp_get_wtime();
+  cout << "Time (Sequential): " << end_time - start_time << " seconds\n\n";
+  start_time = omp_get_wtime();
+  cout << "Parallel Min: " << minval(arr, n) << '\n';
+  cout << "Parallel Max: " << maxval(arr, n) << '\n';
+  cout << "Parallel Sum: " << sum(arr, n) << '\n';
+  cout << "Parallel Avg: " << average(arr, n) << '\n';
+  end_time = omp_get_wtime();
+  cout << "Time (Parallel): " << end_time - start_time << " seconds\n";
+  delete [] arr;
   return 0;
 }
+// Compile: g++ path/to/file/file_name.cpp -fopenmp
+
+// Execute: ./a.out [Linux] or ./a.exe [Windows]
